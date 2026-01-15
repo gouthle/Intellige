ac03542634bc15c40cc4ac7825008bf91b513d83
GOUTH <gouthle@hotmail.com>
2026-01-15 18:38:44 +0100

    Initial Commit

---
+++ Intellige/IntelligeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelligeApp.swift
+//  Intellige
+//
+//  Created by Vladimir Novikov on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct IntelligeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
